2fda6e465b434c409cec410f240d0b1f2857b3b1
Etarious <ojameruayeetaoghene@gmail.com>
2023-02-02 22:34:56 +0100

    The final job, though still undergoing testing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from db import connection
 
+
 ## initialize our tables
 connection.createUsersTable()
 connection.createNotesTable()
@@ -11,24 +12,45 @@ print("""
 result = connection.loginUser()
 
 
+while True:
+    if result == None:
+        print("\n--------------------------------------------------")
+        print("\nUser with that account does not exist. \nDo you want to register?")
+        response = input("Enter 'Y' for Yes or 'N' for No: ").upper()
 
-if result == None:
-    print("\n--------------------------------------------------")
-    print("\nUser with that account does not exist. \nDo you want to register?")
-    response = input("Enter 'Y' for Yes or 'N' for No: ").upper()
-
-    if response == "N":
-        print("Goodbye!")
-        exit()
-    elif response == "Y":
-        register_result = connection.registerUser()
-        print(register_result)
+        if response == "N":
+            print("Goodbye!")
+            exit()
+        elif response == "Y":
+            register_result = connection.registerUser()
+            # print(register_result)
+            if register_result:
+                print("Registration Successful!")
+                # connection.loginUser()
+                print("\nRun the program again.")
+        else:
+        	print("You were to enter either 'Y' or 'N'!")
+        	print("Thank You!")
+        	exit()
     else:
-    	print("You were to enter either 'Y' or 'N'!")
-    	print("Thank You!")
-    	exit()
-        
+        # print("\n-----------------------------------------------------")
+        # print("\nYou are logged in Successfully!")
+
+        # print(result[0])
+        note_data = connection.whatNextDo(result[0])
+
+        # print(note_data)
+
+        if note_data != None:
+            print("\n-------------------------------------------------------")
+            print("\n" + note_data[2].upper())
+            print("\n-------------------------------------------------------")
+            print("\n" + note_data[3])
+            print("\n-------------------------------------------------------")
+            print("\nThe End!")
+        elif note_data == None:
+            print("\nNote not Found!")
 
 
 
-## check if the username and password exist
+    ## check if the username and password exist
--- db/connection.py
+++ db/connection.py
@@ -7,9 +7,8 @@ _db_conn = mysql.connector.connect(
     database="note_taking"
 )
 
-cursor = _db_conn.cursor()
-
 def createUsersTable():
+    cursor = _db_conn.cursor()
     cursor.execute("""
             CREATE TABLE IF NOT EXISTS `users`(
                 `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -21,38 +20,50 @@ def createUsersTable():
                 `date_created` TIMESTAMP
             )
          """)
+    cursor.close()
 
 def createNotesTable():
+    cursor = _db_conn.cursor()
     cursor.execute("""
             CREATE TABLE IF NOT EXISTS `notes`(
                 `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                `author_id` INT NOT NULL,
+                `note_author_id` INT NOT NULL,
                 `note_title` LONGTEXT NOT NULL,
                 `note_content` LONGTEXT NOT NULL,
                 `date_created` TIMESTAMP
             )
          """)
+    cursor.close()
 
 
 def checkIfUserExists(username, password):
+    cursor = _db_conn.cursor()
+    _db_conn.commit()
     cursor.execute("SELECT * FROM `users` WHERE `username` = '"+ username + "' AND `password` ='" +password + "' LIMIT 1")
     for result in cursor:
         if result is not None:
+            cursor.close()
             return result
         else:
+            cursor.close()
             return None
 
 
 def checkIfUsernameIsTaken(username):
+    cursor = _db_conn.cursor()
+    _db_conn.commit()
     cursor.execute("SELECT * FROM `users` WHERE `username` = '" + username + "' LIMIT 1")
     for result in cursor:
         if result != None:
-            return result
+            cursor.close()
+            return True
         else:
+            cursor.close()
             return None
 
 
 def registerUser():
+    cursor = _db_conn.cursor()
     errors = []
     fullname = input("Enter your fullname: ").strip()
     email = input("Enter your email address: ").strip()
@@ -62,58 +73,139 @@ def registerUser():
     password_confirmation = input('Enter password again: ').strip()
 
     if len(fullname) == 0:
-        errors.append("You did not enter your full name")
+        errors.append("\nYou did not enter your full name")
 
     if len(email) == 0:
-        errors.append("You did not enter email")
+        errors.append("\nYou did not enter email")
 
     if len(gender) == 0:
-        errors.append("You did not enter gender")
+        errors.append("\nYou did not enter gender")
 
     if gender != 'male' and gender != 'female' and gender != 'others':
         errors.append("Enter 'male', 'female' or 'others' for gender")
 
     if len(username) == 0:
-        errors.append("You did not enter username: ")
+        errors.append("\nYou did not enter username: ")
     
     if len(password) == 0:
-        errors.append("You did not enter password")
+        errors.append("\nYou did not enter password")
     
     if len(password_confirmation) == 0:
-        errors.append("You did not enter password confirmation")
+        errors.append("\nYou did not enter password confirmation")
     
     if password != password_confirmation:
-        errors.append("Password and Password confirmation must match!")
+        errors.append("\nPassword and Password confirmation must match!")
 
     if len(errors) == 0:
         # there are no errors
-        # It's tim eot check if the username already exists before you register...
-        # print("No errors")
+        # It's time to check if the username already exists before you register...
         result = checkIfUsernameIsTaken(username)
-
-        # cursor.execute("SHOW TABLES")
-        # for tb in cursor:
-        #     print(tb)
         if result == None:
             # The username has not been taken, register the user...
             cursor.execute("INSERT INTO `users` (`fullname`, `email`, `gender`, `username`, `password`) VALUES ('" + fullname + "', '" + email + "', '" + gender + "', '" + username + "', '" + password + "')")
             # print("Registered Successfully!")
-            print(cursor)
-            # for res in cursor:
-            #     if res != None:
-            #         return res
-            #     else:
-            #         return None
+            _db_conn.commit()
+            # print(cursor)
+            cursor.close()
+            return True
     else:
         # there are errors
         for err in errors:
             print(err)
 
-        # registerUser()
-        # print(gender != 'male')
+        cursor.close()
 
 def loginUser():
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-    result = checkIfUserExists(username, password) 
-    return result
+    cursor = _db_conn.cursor()
+    errors = []
+    username = input("Enter your username: ").strip()
+    password = input("Enter your password: ").strip()
+
+    if len(username) == 0:
+        errors.append("\nYou did not enter your username!")
+
+    if len(password) == 0:
+        errors.append("\nYou did not enter your password!")
+
+    if len(errors) == 0:
+
+        result = checkIfUserExists(username, password) 
+        cursor.close()
+        return result
+    else:
+        for err in errors:
+            print(err)
+
+        cursor.close()
+
+def createNote(author_id):
+    cursor = _db_conn.cursor()
+    errors = []
+    note_title = input("Enter Title: ").strip().lower()
+    note_content = input("Enter Content: ").strip()
+
+    if len(note_title) == 0:
+        errors.append("\nPleaese enter a Title for your note!")
+
+    if len(note_content) == 0:
+        errors.append("\nPlease enter the Contents of your note!")
+
+    if len(errors) == 0:
+        # there are no errors
+
+        query = "INSERT INTO `notes` (`note_author_id`, `note_title`, `note_content`) VALUES (%s, %s, %s)"
+        data = (author_id, note_title, note_content)
+        cursor.execute(query, data)
+        _db_conn.commit()
+        return True
+    else:
+        # there are errors
+        for err in errors:
+            print(err)
+
+        cursor.close()
+
+def whatNextDo(author_id):
+    cursor = _db_conn.cursor()
+    whatNext = input("\nTo create new note enter 'N', to read your saved notes enter 'R', and to cancel the program enter 'C': ").upper()
+
+    if whatNext == "N":
+        # print(result[0])
+        note = createNote(author_id)
+
+        if note:
+            print("Note Created Successfully!")
+            cursor.close()
+    elif whatNext == "R":
+        # print("Reading note still under development!")
+        title = input("Enter the title of the note: ").strip().lower()
+
+        if len(title) == 0:
+            cont = input("\nNo title entered! Enter 'T' to try again; Enter 'C' to end program: ").upper()
+
+            if cont == "T":
+                cursor.close()
+            elif cont == "C":
+                print("Thank you for your patronage!")
+                exit()
+            else:
+                print("\nIncorrect inputs!")
+                cursor.close()
+        else:
+            read_query = "SELECT * FROM `notes` WHERE `note_author_id` = '%s' AND `note_title` = '" + title + "' LIMIT 1"
+            read_data = (author_id,)
+            cursor.execute(read_query, read_data)
+            # cursor.execute("SELECT * FROM `notes` WHERE `note_author_id` = '" + author_id + "' AND `note_title` = '" + title + "' LIMIT 1")
+            for result in cursor:
+                if result != None:
+                    cursor.close()
+                    return result
+                else:
+                    cursor.close()
+                    return None
+    elif whatNext == "C":
+        print("\nThank you for your patronage!")
+        exit()
+    else:
+        print("\nInvalid input!")
+        cursor.close()
